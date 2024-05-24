4f36f94140b3234727aff9849463be0edff9e02c
Mathis L'Hôtellier <mathislhotellier@gmail.com>
2024-05-24 11:16:56 +0200

    Define an unconnect userId if user isn't connect, modification for error in console (front)

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,14 +4,12 @@ const { secretKey } = require('../config');
 module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(
-            token,
-            // 'RANDOM_TOKEN_SECRET'
-            secretKey
-        );
-        const userId = decodedToken.userID;
-        req.auth = {
-            userId: userId
+        if (token) {
+            const decodedToken = jwt.verify(token, secretKey);
+            const userId = decodedToken.userID;
+            req.auth = { userId: userId };
+        } else {
+            req.auth = { userId: 'unconnected' };
         }
     next();
     } catch(error) {
