599d8d431589b164cbf5097f3e8e7bea16502fb6
Mathis L'Hôtellier <mathislhotellier@gmail.com>
2024-06-04 14:25:41 +0200

    Add createdAt and lastActivity in userSchema

--- models/User.js
+++ models/User.js
@@ -3,7 +3,9 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  createdAt: { type: Date },
+  lastActivity: { type: Date  }
 });
 
 userSchema.plugin(uniqueValidator);
