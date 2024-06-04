dde7b91f96f2aad0fd0850c638e3a824dcb172da
Mathis L'Hôtellier <mathislhotellier@gmail.com>
2024-06-04 14:49:58 +0200

    Add Regex rule for password, and createdAt in user signin. Add lastActivity in User login

--- controllers/user.js
+++ controllers/user.js
@@ -12,11 +12,19 @@ exports.signup = (req, res, next) => {
         return res.status(400).json({ message: 'Invalid email address.' });
     }
 
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/;
+    const isValidPassword = passwordRegex.test(req.body.password);
+
+    if (!isValidPassword) {
+        return res.status(400).json({ message: 'Password must contain at least 8 characters, including: 1 uppercase letter, 1 lowercase letter and 1 special character.'});
+    }
+
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
         const user = new User({
             email: req.body.email,
-            password: hash
+            password: hash,
+            createdAt: new Date(),
         });
         user.save()
             .then(() => res.status(201).json({ message: 'User created' }))
@@ -35,16 +43,21 @@ exports.login = (req, res, next) => {
                     .then(valid => {
                         if (!valid) {
                             res.status(401).json({ message: 'Incorrect email/password pair.' });
-                        } else {
-                            res.status(200).json({
-                                userId: user._id,
-                                token: jwt.sign(
-                                    { userID: user._id },
-                                    secretKey,
-                                    { expiresIn: '24h' }
-                                )
-                            });
-                        }
+                        } 
+                        const lastActivity = user.lastActivity = Date.now();
+                        user.save()
+                            .then(() => {
+                                res.status(200).json({
+                                    userId: user._id,
+                                    lastActivity: lastActivity,
+                                    token: jwt.sign(
+                                        { userID: user._id },
+                                        secretKey,
+                                        { expiresIn: '24h' }
+                                    )
+                                });
+                            })
+                            .catch(error => res.status(500).json({ error: error.message }));
                     })
                     .catch(error => {
                         res.status(500).json({ error });
