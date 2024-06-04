20ec48dd0dfa50eb1259e91994458c35f6f2aa9c
Mathis L'Hôtellier <mathislhotellier@gmail.com>
2024-06-04 14:51:01 +0200

    Add expiration time limit at 10mn for token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,8 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const User = require('../models/User')
 const { secretKey } = require('../config');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(
@@ -9,6 +10,24 @@ module.exports = (req, res, next) => {
             secretKey
         );
         const userId = decodedToken.userID;
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(401).json({ error: 'User not found. Please log in again.' });
+        }
+
+        const currentTime = Date.now();
+        const lastActivity = user.lastActivity.getTime();
+        const maxDelay = 10 * 60 * 1000; // 10 minutes
+        const inactivityTime = currentTime - lastActivity;
+
+        if (inactivityTime > maxDelay) {
+            return res.status(401).json({ error: 'Session expired due to inactivity. Please log in again.' });
+        }
+
+        user.lastActivity = Date();
+        await user.save();
+
         req.auth = {
             userId: userId
         }
